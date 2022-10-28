d7773ee9fe4037bd99203bc9f84911c17347ffc7
Venkat Sai Ram <heliobvsr2002@gmail.com>
2022-10-29 03:01:42 +0530

    add JS and update formatting

--- readme.md
+++ readme.md
@@ -1,8 +1,19 @@
-1. border-color, font-color(place holder color) for input
+1. border-color for input
 
 ## Learnings
-1. How to set the color of the placeholder value
- ```css:
+
+1. How to style the color of the placeholder value
+
+````css:
 ::placeholder {
-    color: white;
+   color: white;
 }
+
+2. How to remove the border highlight on an input text element
+```css:
+outline: none;
+
+3. num.toFixed(n) , n is number of decimals. this function rounds and gives the value
+
+4. to get n decimals without rounding , mutliply with 1000..n zeros and divide with the same value and apply Math.floor()
+````
--- script.js
+++ script.js
@@ -4,4 +4,22 @@
 1 kilogram = 2.204 pound
 */
 
-const value = document.getElementsByTagName("input")
+const convertBtn = document.querySelector("button");
+const len = document.getElementById("len");
+const vol = document.getElementById("vol");
+const mass = document.getElementById("mass");
+
+convertBtn.addEventListener("click", function () {
+  let inputValue = document.getElementById("input-btn").value;
+  len.textContent = `${inputValue} meters = ${(3.281 * inputValue).toFixed(
+    3
+  )} feet | ${inputValue} feet = ${(inputValue / 3.281).toFixed(3)} meters`;
+  vol.textContent = `${inputValue} liters = ${(0.264 * inputValue).toFixed(
+    3
+  )} gallons | ${inputValue} gallons = ${(inputValue / 0.264).toFixed(
+    3
+  )} liters`;
+  mass.textContent = `${inputValue} kilos = ${(2.204 * inputValue).toFixed(
+    3
+  )} pounds | ${inputValue} pounds = ${(inputValue / 2.204).toFixed(3)} kilos`;
+});
